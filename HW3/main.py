c59768242016fdf7b64ecf8fec5039c575a0ab3e
MissSirius <2267408610@qq.com>
2021-10-25 16:38:07 +0800

    Done

--- HW3/main.py
+++ HW3/main.py
@@ -3,8 +3,7 @@ from matplotlib import pyplot as plt
 import os
 import scipy.optimize
 import cv2
-
-np.set_printoptions(precision=4)
+import time
 
 
 def CPM_DE(projection_matrix):
@@ -98,8 +97,22 @@ def DLT(nd, xyz, uv):
     return L, err
 
 
+def calAndDeEssen(pts1, pts2, K1, K2):
+    f_avg = (K1[0, 0] + K2[0, 0]) / 2
+    pts1, pts2 = np.ascontiguousarray(pts1, np.float32), np.ascontiguousarray(pts2, np.float32)
+
+    pts_l_norm = cv2.undistortPoints(np.expand_dims(pts1, axis=1), cameraMatrix=K1, distCoeffs=None)
+    pts_r_norm = cv2.undistortPoints(np.expand_dims(pts2, axis=1), cameraMatrix=K2, distCoeffs=None)
+
+    E, mask = cv2.findEssentialMat(pts_l_norm, pts_r_norm, focal=1.0, pp=(0., 0.),
+                                   method=cv2.RANSAC, prob=0.999, threshold=3.0 / f_avg)
+    points, R, t, mask_pose = cv2.recoverPose(E, pts_l_norm, pts_r_norm)
+    return E, R, t
+
+
 if __name__ == '__main__':
     # Task1
+    st = time.time()
     print('=' * 15 + 'Task1' + '=' * 15)
     P = np.array([[3.53553e+2, 3.39645e+2, 2.77744e+2, -1.44946e+6],
                   [-1.03528e+2, 2.33212e+1, 4.59607e+2, -6.3252e+5],
@@ -113,9 +126,11 @@ if __name__ == '__main__':
     print(R)
     print('\nC:')
     print(C)
+    print('\nConsumed Time: {}'.format(time.time()-st))
 
     # Task2
-    print('\n\n'+'='*15+'Task2'+'='*15)
+    st = time.time()
+    print('\n\n' + '=' * 15 + 'Task2' + '=' * 15)
     xyz = np.array([[0, 0, 104.2], [180, 0, 104.2], [180, -90, 104.2], [0, 0, 101.5], [0, -1, 0], [175, 0, 0]])
 
     img1 = cv2.imread('./CS284_hw3_data/img1.jpeg')
@@ -149,23 +164,143 @@ if __name__ == '__main__':
     uv3 = [[687, 819], [2735, 1175], [3275, 896], [687, 849], [877, 1929], [2527, 2760]]
 
     L1, err1 = DLT(3, xyz, uv1)
-    K1, _, _ = CPM_DE(L1)
+    K1, R1, C1 = CPM_DE(L1)
     print('Intrinsics Matrix For Img1: ')
     print(K1)
     print('Error For Img1: ', err1)
 
     L2, err2 = DLT(3, xyz, uv2)
-    K2, _, _ = CPM_DE(L2)
+    K2, R2, C2 = CPM_DE(L2)
     print('\nIntrinsics Matrix For Img2: ')
     print(K2)
     print('Error For Img2: ', err2)
 
     L3, err3 = DLT(3, xyz, uv3)
-    K3, _, _ = CPM_DE(L3)
+    K3, R3, C3 = CPM_DE(L3)
     print('\nIntrinsics Matrix For Img3: ')
     print(K3)
     print('Error For Img3: ', err3)
 
-    print('\nError between K1 and K2: ', np.abs(K1-K2)[:, :2].sum())
-    print('Error between K2 and K3: ', np.abs(K2-K3)[:, :2].sum())
-    print('Error between K3 and K1: ', np.abs(K3-K1)[:, :2].sum())
+    print('\nError between K1 and K2: ', np.abs(K1 - K2)[:, :2].sum())
+    print('Error between K2 and K3: ', np.abs(K2 - K3)[:, :2].sum())
+    print('Error between K3 and K1: ', np.abs(K3 - K1)[:, :2].sum())
+    print('\nConsumed Time: {}'.format(time.time() - st))
+
+    # Task3
+    st = time.time()
+    print('\n\n' + '=' * 15 + 'Task3' + '=' * 15)
+    orb = cv2.ORB_create()
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+
+    img1 = cv2.imread('./CS284_hw3_data/img1.jpeg', 0)
+    img2 = cv2.imread('./CS284_hw3_data/img2.jpeg', 0)
+    img3 = cv2.imread('./CS284_hw3_data/img3.jpeg', 0)
+
+    kp1, des1 = orb.detectAndCompute(img1, None)
+    kp2, des2 = orb.detectAndCompute(img2, None)
+    kp3, des3 = orb.detectAndCompute(img2, None)
+
+    matches = bf.match(des1, des2)
+    matches = sorted(matches, key=lambda x: x.distance)
+    min_match = matches[0].distance
+    matches = [match for match in matches if match.distance <= max(2 * min_match, 30.0)]
+    img_match12 = cv2.drawMatches(img1, kp1, img2, kp2, matches, None, flags=2)
+    cv2.imwrite('match_img12.jpeg', img_match12)
+
+    matches = bf.match(des2, des1)
+    matches = sorted(matches, key=lambda x: x.distance)
+    min_match = matches[0].distance
+    matches = [match for match in matches if match.distance <= max(2 * min_match, 30.0)]
+    img_match21 = cv2.drawMatches(img2, kp2, img1, kp1, matches, None, flags=2)
+    cv2.imwrite('match_img21.jpeg', img_match21)
+
+    matches = bf.match(des1, des3)
+    matches = sorted(matches, key=lambda x: x.distance)
+    min_match = matches[0].distance
+    matches = [match for match in matches if match.distance <= max(2 * min_match, 30.0)]
+    img_match13 = cv2.drawMatches(img1, kp1, img3, kp3, matches, None, flags=2)
+    cv2.imwrite('match_img13.jpeg', img_match13)
+
+    matches = bf.match(des3, des1)
+    matches = sorted(matches, key=lambda x: x.distance)
+    min_match = matches[0].distance
+    matches = [match for match in matches if match.distance <= max(2 * min_match, 30.0)]
+    img_match31 = cv2.drawMatches(img3, kp3, img1, kp1, matches, None, flags=2)
+    cv2.imwrite('match_img31.jpeg', img_match31)
+
+    matches = bf.match(des2, des3)
+    matches = sorted(matches, key=lambda x: x.distance)
+    min_match = matches[0].distance
+    matches = [match for match in matches if match.distance <= max(2 * min_match, 30.0)]
+    img_match23 = cv2.drawMatches(img2, kp2, img3, kp3, matches, None, flags=2)
+    cv2.imwrite('match_img23.jpeg', img_match23)
+
+    matches = bf.match(des3, des2)
+    matches = sorted(matches, key=lambda x: x.distance)
+    min_match = matches[0].distance
+    matches = [match for match in matches if match.distance <= max(2 * min_match, 30.0)]
+    img_match32 = cv2.drawMatches(img3, kp3, img2, kp2, matches, None, flags=2)
+    cv2.imwrite('match_img32.jpeg', img_match32)
+
+    match12_p1s = list()
+    match12_p2s = list()
+    for i in range(len(matches)):
+        match12_p1s.append(kp1[matches[i].queryIdx].pt)
+        match12_p2s.append(kp2[matches[i].queryIdx].pt)
+    E12, R12, t12 = calAndDeEssen(match12_p1s, match12_p2s, K1, K2)
+
+    match13_p1s = list()
+    match13_p2s = list()
+    for i in range(len(matches)):
+        match13_p1s.append(kp1[matches[i].queryIdx].pt)
+        match13_p2s.append(kp2[matches[i].queryIdx].pt)
+    E13, R13, t13 = calAndDeEssen(match13_p1s, match13_p2s, K1, K3)
+
+    match23_p1s = list()
+    match23_p2s = list()
+    for i in range(len(matches)):
+        match23_p1s.append(kp1[matches[i].queryIdx].pt)
+        match23_p2s.append(kp2[matches[i].queryIdx].pt)
+    E23, R23, t23 = calAndDeEssen(match23_p1s, match23_p2s, K2, K3)
+
+    match21_p1s = list()
+    match21_p2s = list()
+    for i in range(len(matches)):
+        match21_p1s.append(kp1[matches[i].queryIdx].pt)
+        match21_p2s.append(kp2[matches[i].queryIdx].pt)
+    E21, R21, t21 = calAndDeEssen(match21_p1s, match21_p2s, K2, K1)
+
+    match31_p1s = list()
+    match31_p2s = list()
+    for i in range(len(matches)):
+        match31_p1s.append(kp1[matches[i].queryIdx].pt)
+        match31_p2s.append(kp2[matches[i].queryIdx].pt)
+    E31, R31, t31 = calAndDeEssen(match31_p1s, match31_p2s, K3, K1)
+
+    match32_p1s = list()
+    match32_p2s = list()
+    for i in range(len(matches)):
+        match32_p1s.append(kp1[matches[i].queryIdx].pt)
+        match32_p2s.append(kp2[matches[i].queryIdx].pt)
+    E32, R32, t32 = calAndDeEssen(match32_p1s, match32_p2s, K3, K2)
+
+    A1 = np.zeros((3, 4))
+    A1[:, 0:3] = R1
+    A1[:, 3:4] = C1.reshape((3, 1))
+
+    A2 = np.zeros((3, 4))
+    A2[:, 0:3] = R2
+    A2[:, 3:4] = C2.reshape((3, 1))
+
+    A3 = np.zeros((3, 4))
+    A3[:, 0:3] = R3
+    A3[:, 3:4] = C3.reshape((3, 1))
+
+    print('Transform Matrix Error for Img1-Img2: ', np.abs(np.dot(A2, np.linalg.pinv(A1)) - R12).sum())
+    print('Transform Matrix Error for Img1-Img3: ', np.abs(np.dot(A3, np.linalg.pinv(A1)) - R13).sum())
+    print('Transform Matrix Error for Img2-Img3: ', np.abs(np.dot(A3, np.linalg.pinv(A2)) - R23).sum())
+    print('Transform Matrix Error for Img2-Img1: ', np.abs(np.dot(A1, np.linalg.pinv(A2)) - R21).sum())
+    print('Transform Matrix Error for Img3-Img1: ', np.abs(np.dot(A1, np.linalg.pinv(A3)) - R31).sum())
+    print('Transform Matrix Error for Img3-Img2: ', np.abs(np.dot(A2, np.linalg.pinv(A3)) - R32).sum())
+    print('\nConsumed Time: {}'.format(time.time() - st))
+
