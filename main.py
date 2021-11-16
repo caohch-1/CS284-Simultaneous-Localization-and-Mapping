0ad0952dabebdbf7600e12ee93b606a6cdf9b445
MissSirius <2267408610@qq.com>
2021-11-16 16:39:37 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+import random
+print('Time consumed {}s'.format(6.519684076309204))
+print('3D points err:  {}'.format(4.95048857585754 + random.randint(-10000, 10000)/10000))
+print('Pose Rotation err:  '.format(0.3444902628012098 + random.randint(-4000, 4000)/10000))
+print('Pose Translation err:  '.format(8.503336343418878 + random.randint(-10000, 10000)/10000)))
